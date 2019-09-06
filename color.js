db52fb8552b7c0d5075d379b1f67208f14aadcba
Kimochii-x3 <38957871+Kimochii-x3@users.noreply.github.com>
2019-09-06 01:11:35 +0100

    Add files via upload

---
+++ color.js
@@ -0,0 +1,149 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: 'color',
+  description: 'creates a color role for yourself, works only with servers that have roles with the "default" color',
+  usage: 'fl.color #<hexcode> /provide no hexcode to see your color hexcode or mention someone to see their role hexcode/',
+  args: false,
+  execute(bot, message, args, option) {
+    if (!message.guild.me.permissions.has('ADMINISTRATOR', true)) {return message.reply("insufficient permissions, add 'ADMINISTRATOR' perms to my role 'FutatsuLollies'");}
+    else if (message.guild.me.permissions.has('ADMINISTRATOR', true)) {
+      let phC = message.guild.roles.find(role => role.name == "▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇");
+      if(!phC){
+        try {
+          message.guild.createRole({name:'▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇'}, "Creating a placeholder role for the colors");
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      else if (phC) {
+      let thisGuild = message.guild;
+      let mAuthorID = message.author.id;
+      // let rqUser = message.guild.member(id);
+      let clr = message.member.displayHexColor;
+      let hexCode = args[0];
+      let idOthers = message.mentions.members.first();
+      let roleColor = message.guild.roles.find(role => role.name === "USER-"+mAuthorID);
+      let placeholder = message.guild.roles.find(role => role.name == "▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇");
+
+      let rolePreview = new Discord.RichEmbed()
+      .setDescription("**Your role will look like this:**\n"+placeholder+"\n"+placeholder+"\n"+placeholder+"\n**Do you want to change your color?**")
+      .setColor(clr)
+
+      let rolePreview1 = new Discord.RichEmbed()
+      .setDescription("**Your role will look like this:**\n"+placeholder+"\n"+placeholder+"\n"+placeholder)
+      .setColor(clr)
+
+      let reply1 = new Discord.RichEmbed()
+      .setDescription("**Role Created**")
+      .setColor(clr)
+
+      let reply2 = new Discord.RichEmbed()
+      .setDescription("**Role Updated**")
+      .setColor(clr)
+
+
+      if(!idOthers)
+      {
+        if(!roleColor)
+        {
+          if(!hexCode)
+          {
+            return message.channel.send("No role found.");
+          }
+          else if(!hexCode.startsWith("#"))
+          {
+            return message.channel.send("Incorrect code, example: fl.color #ff00ff");
+          }
+          else if(hexCode.startsWith("#"))
+          {
+            thisGuild.createRole({name: "USER-"+mAuthorID, color: hexCode, position: placeholder.calculatedPosition+1}).then(role =>
+              {console.log(`Role created with name: ${role.name} & color: ${role.color}`);
+              message.channel.send(rolePreview1);
+              return rqUser.addRole(role.id)
+            }).catch(console.error);
+          }
+        }
+        else if(roleColor)
+        {
+          if(!hexCode)
+          {
+            return message.channel.send("Your role hex code is: "+roleColor.hexColor);
+          }
+          else if(hexCode === "remove")
+          {
+            roleColor.delete().catch(console.error);
+            return message.channel.send(roleColor+" was deleted.");
+          }
+          else if(!hexCode.startsWith("#"))
+          {
+            return message.channel.send("Incorrect code, example: fl.color #ff00ff");
+          }
+          else if(hexCode.startsWith("#"))
+          {
+            placeholder.setColor(hexCode).then(roleColorChange => {
+              console.log(`Changed the color of role ${roleColorChange.name} to ${roleColorChange.color}`);
+              //placeholder.setColor(hexCode);
+               const filter = (reaction, user) =>
+               {
+                 return ['✅', '❌'].includes(reaction.emoji.name) && user.id === mAuthorID;
+               };
+               message.channel.send(rolePreview).then(async (message) =>
+               {
+                 await message.react('✅');
+                 await message.react('❌');
+                 await message.awaitReactions(filter, {max: 1, time: 8000, errors: ['time'] }).then(reacts => {
+                   const reaction = reacts.first();
+                   if(reaction.emoji.name === '✅')
+                   {
+                     const latestEmbed = message.embeds[0];
+                     const acceptEmbed = new Discord.RichEmbed(latestEmbed)
+                     .setDescription("**Role Updated**")
+                     .setColor(clr);
+                     message.edit(acceptEmbed).catch(o_O => {})
+                     message.clearReactions().catch(o_O => {})
+                     return roleColor.setColor(hexCode);
+                   }
+                   else if(reaction.emoji.name === '❌')
+                   {
+                     const latestEmbed = message.embeds[0];
+                     const cancelEmbed = new Discord.RichEmbed(latestEmbed)
+                     .setDescription("**Canceled**")
+                     .setColor(clr);
+                     message.clearReactions().catch(o_O => {})
+                     return message.edit(cancelEmbed).catch(o_O => {});
+                   }
+                   // else if(reacts.size === 0)
+                   // {
+
+                   // }
+                 }).catch(error => {
+                      const latestEmbed = message.embeds[0];
+                      const noResponseEmbed = new Discord.RichEmbed(latestEmbed)
+                      .setDescription("**Times Up**")
+                      .setColor(clr);
+                      message.clearReactions().catch(o_O => {})
+                      return message.edit(noResponseEmbed).catch(o_O => {});
+                    });
+               }).catch(console.error);
+            }).catch(console.error);
+          }
+        }
+      }
+      else if(idOthers)
+      {
+        if(hexCode === "<@!"+idOthers.id+">")
+          {
+            let roleColorOthers = message.guild.roles.find(role => role.name === "USER-"+idOthers.id);
+            if(!roleColorOthers){
+              return message.channel.send("Role hex code is: "+idOthers.displayHexColor);
+            }
+            else if (roleColorOthers) {
+              return message.channel.send("Role hex code is: "+roleColorOthers.hexColor);
+            }
+          }
+      }
+    }
+    }
+  },
+};
---
+++ quote.js
@@ -0,0 +1,147 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: 'quote',
+  description: 'quotes a user, you can make fake quotes or real ones (real ones include "Jump to message")',
+  usage: 'fl.quote user @<member>/<USER id> <content> ; fl.quote message <MESSAGE id> /#<channel name>, required only when quoting outside of the channel/',
+  args: true,
+  async execute(bot, message, args, option) {
+    // let modRole = message.guild.roles.find(role => role.id === cache.modRole);
+    let mAuthor = message.author.id;
+    let rqUser = message.member;
+    let quoteFrom = message.mentions.members.first();
+    let channelToQuoteFrom = message.mentions.channels.first();
+    let mContent = message.content.split(/\s+/g) //substring(message.content.indexOf(" ")+args0.length+2, message.content.length);
+    //
+    let args1 = mContent[1];
+    let args2 = mContent[2];
+    let args3 = mContent[3];
+    switch (args1)
+    {
+      case "user":
+        if(quoteFrom)
+        {
+          let mContent2 = mContent.slice(3).join(" ");
+          let clr = message.guild.member(quoteFrom).displayHexColor;
+          message.delete().catch();
+          let quote = new Discord.RichEmbed()
+          .setAuthor(bot.users.get(quoteFrom.id).tag, bot.users.get(quoteFrom.id).displayAvatarURL)
+          .setDescription(mContent2)
+          .setTimestamp()
+          .setColor(clr)
+          return message.channel.send(quote)
+        }
+        else if(!quoteFrom)
+        {
+          let mContent2 = mContent.slice(3).join(" ");
+          let clr = message.guild.member(message.guild.member(args2)).displayHexColor;
+          message.delete().catch();
+          let quote = new Discord.RichEmbed()
+          .setAuthor(bot.users.get(args2).tag, bot.users.get(args2).displayAvatarURL)
+          .setDescription(mContent2)
+          .setTimestamp()
+          .setColor(clr)
+          return message.channel.send(quote)
+        }
+      case "message":
+        if(channelToQuoteFrom)
+        {
+          let mContent2 = mContent.slice(4).join(" ");
+          let fetchedMsgUser = await channelToQuoteFrom.fetchMessage(args3).then(message => message.author.id).catch(console.error);
+          let fetchedMsgContent = await channelToQuoteFrom.fetchMessage(args3).then(message => message.content).catch(console.error);
+          let fetchedMsgFiles = await channelToQuoteFrom.fetchMessage(args3).then(message => message.attachments.map(attachments => attachments.proxyURL)).catch(console.error);
+          let fetchedMsgLink = await channelToQuoteFrom.fetchMessage(args3).then(message => message.url).catch(console.error);
+          let clr = message.guild.member(message.guild.member(fetchedMsgUser)).displayHexColor;
+          message.delete().catch();
+          if(fetchedMsgFiles.length == 0)
+          {
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+          else if(fetchedMsgFiles.length == 1 && (fetchedMsgFiles.toString().includes('.png')||fetchedMsgFiles.toString().includes('.gif')||fetchedMsgFiles.toString().includes('.jpg')||fetchedMsgFiles.toString().includes('.jpeg')||fetchedMsgFiles.toString().includes('.tiff')||fetchedMsgFiles.toString().includes('.tif')||fetchedMsgFiles.toString().includes('.bmp')))
+          {
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setImage(fetchedMsgFiles.toString())
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+          else if(fetchedMsgFiles.length == 1 && !(fetchedMsgFiles.toString().includes('.png')||fetchedMsgFiles.toString().includes('.gif')||fetchedMsgFiles.toString().includes('.jpg')||fetchedMsgFiles.toString().includes('.jpeg')||fetchedMsgFiles.toString().includes('.tiff')||fetchedMsgFiles.toString().includes('.tif')||fetchedMsgFiles.toString().includes('.bmp')))
+          {
+            fetchedMsgFiles = fetchedMsgFiles.toString().replace(/,/g, "\n");
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n**File format not an image: **"+fetchedMsgFiles+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+          else if(fetchedMsgFiles.length >= 2)
+          {
+            fetchedMsgFiles = fetchedMsgFiles.toString().replace(/,/g, "\n");
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n**Multiple files: **"+fetchedMsgFiles+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+        }
+        else if(!channelToQuoteFrom)
+        {
+          let mContent2 = mContent.slice(3).join(" ");
+          let fetchedMsgUser = await message.channel.fetchMessage(args2).then(message => message.author.id).catch(console.error);
+          let fetchedMsgContent = await message.channel.fetchMessage(args2).then(message => message.content).catch(console.error);
+          let fetchedMsgFiles = await message.channel.fetchMessage(args2).then(message => message.attachments.map(attachments => attachments.proxyURL)).catch(console.error);
+          let fetchedMsgLink = await message.channel.fetchMessage(args2).then(message => message.url).catch(console.error);
+          let clr = message.guild.member(message.guild.member(fetchedMsgUser)).displayHexColor;
+          message.delete().catch();
+          if(fetchedMsgFiles.length == 0)
+          {
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+          else if(fetchedMsgFiles.length == 1 && (fetchedMsgFiles.toString().includes('.png')||fetchedMsgFiles.toString().includes('.gif')||fetchedMsgFiles.toString().includes('.jpg')||fetchedMsgFiles.toString().includes('.jpeg')||fetchedMsgFiles.toString().includes('.tiff')||fetchedMsgFiles.toString().includes('.tif')||fetchedMsgFiles.toString().includes('.bmp')))
+          {
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setImage(fetchedMsgFiles.toString())
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+          else if(fetchedMsgFiles.length == 1 && !(fetchedMsgFiles.toString().includes('.png')||fetchedMsgFiles.toString().includes('.gif')||fetchedMsgFiles.toString().includes('.jpg')||fetchedMsgFiles.toString().includes('.jpeg')||fetchedMsgFiles.toString().includes('.tiff')||fetchedMsgFiles.toString().includes('.tif')||fetchedMsgFiles.toString().includes('.bmp')))
+          {
+            fetchedMsgFiles = fetchedMsgFiles.toString().replace(/,/g, "\n");
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n**File format not an image: **"+fetchedMsgFiles+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+          else if(fetchedMsgFiles.length >= 2)
+          {
+            fetchedMsgFiles = fetchedMsgFiles.toString().replace(/,/g, "\n");
+            let quote = new Discord.RichEmbed()
+            .setAuthor(bot.users.get(fetchedMsgUser).tag, bot.users.get(fetchedMsgUser).displayAvatarURL)
+            .setDescription(fetchedMsgContent+"\n**Multiple files: **"+fetchedMsgFiles+"\n"+"\n"+"[Jump to message]("+fetchedMsgLink+")")
+            .setTimestamp()
+            .setColor(clr)
+            return message.channel.send(quote);
+          }
+        }
+    }
+  },
+};
