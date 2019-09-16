2f88a6cf26da5817dbc6c6cdd39701c56a2227d7
Kimochii-x3 <38957871+Kimochii-x3@users.noreply.github.com>
2019-09-16 02:10:22 +0100

    Add files via upload

--- flmain.js
+++ flmain.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const ms = require("ms");
 const fs = require("fs");
 const snekfetch = require("snekfetch");
-const { prefix, token } = require("./botconf.json");
+const mysql = require("mysql");
+const { token } = require("./botconf.json");
 const bot = new Discord.Client();
 let commands = new Discord.Collection();
 let botOwner = bot.users.get("254349985963835393");
@@ -10,7 +11,6 @@ const cmdFiles = fs.readdirSync(__dirname+'/cmd').filter(file => file.endsWith('
 for (const file of cmdFiles) {
   const cmd = require(__dirname+`/cmd/${file}`);
   commands.set(cmd.name, cmd);
-  // console.log(cmd.execute);
 }
 fs.readdir(__dirname+'/events', (err, files) => {
   if(err) return console.log(err);
@@ -20,48 +20,84 @@ fs.readdir(__dirname+'/events', (err, files) => {
     let eventName = file.split(".")[0];
     bot.on(eventName, event.bind(null, bot));
   })
-  // console.log("All events loaded & all commands should be loaded");
 });
+
+
+db.connect(err =>{
+  if(err) return console.log(err);
+  console.log('DB connected');
+});
+
 bot.on("ready", function(){
   var tn = new Date();
   console.log(`${bot.user.username} is online.\n[`+tn.toString()+`]`);
-  bot.user.setActivity("fl.help 4 cmds", {type: "PLAYING"});
-  bot.channels.get("618929645173342210").send("Up and running\n"+tn.toString());
+  bot.user.setActivity(`fl.help / ${bot.guilds.size} servers`, {type: "WATCHING"});
+  let mainBotChannel = bot.channels.get("618929645173342210");
+  mainBotChannel.send("Up and running\n"+tn.toString());
   let guildNames = [];
   let guildLookups = bot.guilds.map(g => `${g.name} \+ ${g.id}`);
   for (const g of guildLookups){
-  guildNames.push(g);}
+  guildNames.push(g);
+  }
+  // setInterval(() => {
+  //   snekfetch.post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
+  //     .set('Authorization', 'DBLtoken')
+  //     .send({ server_count: bot.guilds.size })
+  //     .then(() => console.log('Updated discordbots.org stats.'))
+  //     .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
+  // }, 3600000)
+  // mainBotChannel.send(guildNames);
   // console.log(guildNames);
 });
+
 bot.on("error", console.error);
 
 bot.on("message", async message =>
 {
-  if(message.author.bot) return;
-  if(!message.content.startsWith(prefix)) return;
-  if(message.channel.type === 'dm') return;
-  let args = message.content.slice(prefix.length).split(/ +/);
-  let cmdName = args.shift().toLowerCase();
-  let option = message.content.slice(prefix.length+cmdName.length).split(/-+/);
-  if(!commands.has(cmdName)) return;
-  let cmd = commands.get(cmdName);
-  try {
-     if(cmd.args && !args.length){
-       return message.channel.send('No args provided');
-     }
-     else {
-       cmd.execute(bot, message, args, option, commands);
-     }
-  } catch (error) {
-    console.error(error);
-    message.reply("Issue occured while trying to execute the command.")
-  }
+  db.query(`SELECT prefix FROM serverInfo WHERE serverID = ${message.guild.id}`, (err, rows) => {
+    if (err) throw err;
+    let prefix = rows[0].prefix;
+    // console.log(prefix1);
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(message.channel.type === 'dm') return;
+    let args = message.content.slice(prefix.length).split(/ +/);
+    let cmdName = args.shift().toLowerCase();
+    let option = message.content.slice(prefix.length+cmdName.length).split(/-+/);
+    if(!commands.has(cmdName)) return;
+    let cmd = commands.get(cmdName);
+    try {
+       if(cmd.args && !args.length){
+         return message.channel.send('No args provided');
+       }
+       else {
+         cmd.execute(bot, message, args, option, commands, prefix, db);
+       }
+    } catch (error) {
+      console.error(error);
+      message.reply("Issue occured while trying to execute the command.")
+    }
+  });
 
 
 
 
 });
-bot.on("guildCreate", async guild => {});
+// bot.on("debug", m => console.log(m));
+bot.on("guildCreate", async guild => {
+  // console.log(guild);
+  db.query(`SELECT * FROM serverInfo WHERE serverID = '${guild.id}'`, (err, rows) => {
+    if (err) return console.log(err);
+    let sql;
+    // let serverIDexists = `SELECT EXISTS(SELECT * FROM serverInfo WHERE serverID=${guild.id})`;
+    // console.log(serverIDexists);
+    if(rows.length < 1){
+      sql = `INSERT INTO serverInfo (serverID) VALUES ('${guild.id}')`
+    }
+    // if (sql = null) {return;}
+    db.query(sql, console.log);
+  });
+});
 bot.on("emojiCreate", async emoji => {});
 bot.on("emojiUpdate", async (oldEmoji, newEmoji) => {});
 bot.on("emojiDelete", async emoji => {});
